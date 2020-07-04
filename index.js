f5564c1cebb3314f7c823592a0e7bd40adab0fe1
jingling jin <jljin0524@gmail.com>
2020-07-04 22:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+var array = []
+var musi = ["John"]
+var ins = ["guitar"]
+function theBeatlesPlay(musi,ins){
+  for (let i = -1; i < musi.length; i++){
+  array.push(`${musi[i]} plays ${ins[i]}`)
+  return array
+  }
+}
+//theBeatlesPlay("John","guitar")
